c0ec1b16b805cb35f2b0171fe646204d75010f82
Roy Hyunjin Han <starsareblueandfaraway@gmail.com>
2013-03-19 06:42:52 -0700

    Added overwriteFirst

--- filter.py
+++ filter.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 import argparse
 import datetime
+import shutil
+import sys
+from tempfile import mkstemp
 from tzlocal import get_localzone
 
 from goalIO import GoalFactory, load_whenIO, STATUS_DONE
 
 
-def run(sourcePaths, showAll):
+def run(sourcePaths, showAll, overwriteFirst=False):
+    if overwriteFirst:
+        temporaryPath = mkstemp()[1]
+        sys.stdout = open(temporaryPath, 'wt')
     now = datetime.datetime.now()
     print '# %s %s' % (get_localzone(), now.strftime('%-m/%-d/%Y'))
     for sourcePath in sourcePaths:
@@ -19,6 +25,8 @@ def run(sourcePaths, showAll):
                     goal.start = now
                 if showAll or goal.status < STATUS_DONE:
                     print goal.format('%(leadspace)s%(status)s%(text)s%(when)s')
+    if overwriteFirst:
+        shutil.move(temporaryPath, sourcePaths[0])
 
 
 if __name__ == '__main__':
@@ -29,5 +37,8 @@ if __name__ == '__main__':
     argumentParser.add_argument(
         '-a', '--all', action='store_true',
         help='show completed and cancelled goals too')
+    argumentParser.add_argument(
+        '-u', '--update', action='store_true',
+        help='overwrite first file with output')
     arguments = argumentParser.parse_args()
-    run(arguments.sourcePaths, arguments.all)
+    run(arguments.sourcePaths, arguments.all, arguments.update)
