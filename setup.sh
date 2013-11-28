bbd606378544bb9a34c4eb9b1269ac5e10ab3c75
Roy Hyunjin Han <rhh@crosscompute.com>
2013-11-28 13:56:22 -0800

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-pip install --upgrade whenIO
-pip install --upgrade google-api-python-client
+pip install -U whenIO 
+pip install -U google-api-python-client python-gflags
 if [ -d "$HOME/.scripts" ]; then 
+    ln -sf `pwd`/filter.py ~/.scripts
     ln -sf `pwd`/schedule.py ~/.scripts
     ln -sf `pwd`/pinpoint.py ~/.scripts
-    ln -sf `pwd`/filter.py ~/.scripts
 fi
