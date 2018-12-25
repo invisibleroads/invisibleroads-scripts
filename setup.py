b4cedd344d02134ba879e6d5641c7392a41be099
Roy Hyunjin Han <rhh@crosscompute.com>
2018-12-25 08:22:56 -0500

    Add MANIFEST

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
     'README.rst', 'CHANGES.rst'])
 setup(
     name='invisibleroads-scripts',
-    version='2.0.0',
+    version='2.0.1',
     description='Command-line scripts for managing your goals',
     long_description=DESCRIPTION,
     classifiers=[
@@ -24,7 +24,7 @@ setup(
     url='https://github.com/invisibleroads/invisibleroads-scripts',
     keywords='invisibleroads',
     packages=find_packages(),
-    include_package_data=False,
+    include_package_data=True,
     zip_safe=True,
     install_requires=[
         'invisibleroads',
